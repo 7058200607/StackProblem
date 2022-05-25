9633dc483022a5d20de1f318a046b8c410fd16a6
vrushali joshi <vrushalijoshi589@mail.com>
2022-05-25 14:53:57 +0530

    [200~Ability to create a Stack of

---
+++ StackProblem/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackProblem
+{
+    public class program
+    {
+        public static void Main(string[] args) 
+        {
+            Console.WriteLine("Do Stack Operation!");
+            StacksProblem stacksProblem = new StacksProblem();
+            stacksProblem.push(70);
+            stacksProblem.push(30);
+            stacksProblem.push(56);
+            stacksProblem.display();
+
+        }
+
+
+    }
+}
---
+++ StackProblem/StacksProblem.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackProblem
+{
+    public class StacksProblem
+    {
+        public Node top;
+        public StacksProblem()  
+        {
+            this.top = null;
+
+        }
+        internal void push(int value) 
+        {
+            Node node = new Node(value);
+            if(this.top != null) 
+            {
+                node.next = null;
+            }
+            else 
+            {
+                node.next = this.top;
+            }
+            this.top = node;
+            Console.WriteLine("{0} pushed into stack", value);
+
+           
+        }
+        internal void display() 
+        {
+            Node temp =this.top;
+            while(temp != null) 
+            {
+                Console.WriteLine(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+
+    }
+}
